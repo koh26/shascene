b4e627b892790f3bd3acbc6ff567211197c6a69e
koh26 <sol.van7110@gmail.com>
2020-12-09 18:51:13 +0900

    postsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_072224) do
+ActiveRecord::Schema.define(version: 2020_12_09_020410) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "about", null: false
+    t.date "day"
+    t.string "city"
+    t.string "number"
+    t.string "building"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_12_08_072224) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
