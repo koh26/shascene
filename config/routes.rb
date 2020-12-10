ea61bf9664f80e8cea16176e49f56145857db490
koh26 <sol.van7110@gmail.com>
2020-12-10 18:41:36 +0900

    mapコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # get 'maps/index'
+  # get 'maps/show'
   devise_for :users
-  root to: "posts#index"
+  root to: "maps#index"
   resources :posts, only: [:new, :create]
 end
