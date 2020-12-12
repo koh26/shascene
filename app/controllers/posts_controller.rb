1c1d8241672afbd2d7ef8d7b9e6a2b1c5014e818
koh26 <sol.van7110@gmail.com>
2020-12-12 23:10:41 +0900

    投稿画面画面への遷移

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,20 @@
 class PostsController < ApplicationController
 
   def new
-    @posts = Post.new
+    @city = City.find(params[:map_id])
+    @post = Post.new
+    # binding.pry
   end
   
   def create
+    @city = City.find(params[:map_id])
+    @post = Post.new(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :title, :about, :day, :city, :number, :building).merge(user_id: current_user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # get 'maps/show'
   devise_for :users
   root to: "maps#index"
-  resources :maps, only: [:index, :show]
-  resources :posts, only: [:new, :create]
+  resources :maps, only: [:index, :show] do
+    resources :posts, only: [:new, :create]
+  end
+
 end
