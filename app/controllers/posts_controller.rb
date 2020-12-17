fcbb0272ecf41ab946872ac8d0ba956e539f62df
koh26 <sol.van7110@gmail.com>
2020-12-17 19:16:04 +0900

    コメントフォーム作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
   end
 
   def edit
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to: city
-  belongs_to: user
-  belongs_to: post
+  belongs_to :city
+  belongs_to :user
+  belongs_to :post
 
   validates :text, presence: true
 end
