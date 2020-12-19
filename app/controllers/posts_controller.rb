7b6d8f33249392544723832f578d32ac9d781bba
koh26 <sol.van7110@gmail.com>
2020-12-20 02:46:52 +0900

    検索機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def search
+    @post = Post.search(params[:keyword])
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :maps, only: [:index, :show] do
     resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
       resources :comments, only: [:create]
+      collection do
+        get 'search'
+      end
     end
   end
 
