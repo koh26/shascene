73d5f9e36df9145c6480fc7dfd2d343af216f226
koh26 <sol.van7110@gmail.com>
2020-12-30 22:45:35 +0900

    commentモデルテストコード

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.create(:post)
+    @comment = FactoryBot.create(:comment, user_id: @user.id, post_id: @post.id)
+  end
+
+  describe 'コメント投稿' do
+
+    context 'コメントがうまくいくとき' do
+
+      it 'コメントが入力されていれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+      
+    end
+
+    context 'コメントがうまくいかないとき' do
+
+      it 'コメントが入力されていないと投稿できない' do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+      
+    end
   
+  end
+
 end
